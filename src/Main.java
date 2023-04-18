eb739cac0207ce85ca352dd7c76eab052be2bbc5
maximizzar <maximizzar@web.de>
2023-04-18 12:32:32 +0200

    week16 update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-package week16;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumOfSquares(new double[] { 1., 1., 1. }));
-    }
-    public double sumOfSquares(double[] x) {
-        double sum = 0;
-        for (int i = 0; i < x.length; i++) {
-            sum += x[i] * x[i];
-        }
-        return sum;
+        new Week16().test();
     }
 }
--- src/Week16.java
+++ src/Week16.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Week16 {
     private Integer task = 1;
     public void test() {
@@ -9,13 +11,31 @@ public class Week16 {
         System.out.println(sumOfSquares(new double[] { 1., 2., 3. }));
 
         printHeading();
-        //System.out.println(sumOfColOne(new double[][] {}));
+        System.out.println(sumOfColOne(new double[][] {}));
         System.out.println(sumOfColOne(new double[][] { { 1., 1., 1. }, { 2., 2., 2. }, { 3., 3., 3. } }));
         System.out.println(sumOfColOne(new double[][] { { 1., 2. }, { 1., 2. }, { 1., 2. } }));
         System.out.println(sumOfColOne(new double[][] { { -3.0, 1.0, 2.0, 3.0 } }));
+        System.out.println(sumOfColOne(new double[][] { { 1., 2. }, { 1., 0. } }));
+        System.out.println(sumOfColOne(new double[][] { { -5., 0. }, { -1., 1. }, { -2., 2. } }));
 
+        printHeading();
+        System.out.println(sumOfRow2(new double[][] {}));
+        System.out.println(sumOfRow2(new double[][] { { 1., 1., 1. }, { 2., 2., 2. }, { 3., 3., 3. } }));
+
+        printHeading();
+        System.out.println(sumOfMatrix(new double[][] {}));
+        System.out.println(sumOfMatrix(new double[][] { { 1., 1., 1. }, { 2., 2., 2. }, { 3., 3., 3. } }));
+
+        printHeading();
+
+        printHeading();
+
+        printHeading();
+
+        printHeading();
+        System.out.println(Arrays.deepToString(linearClassifier(new double[][] {}, new double[] { 1.0, 0.0 })));
+        System.out.println(Arrays.deepToString(linearClassifier(new double[][] { { 1., 1. }, { 2., 2. }, { 3., 3. } }, new double[] { 1.0, 0.0 })));
     }
-
     public double sumOfSquares(double[] array) {
         double sum = 0;
         for (double value : array) {
@@ -23,12 +43,46 @@ public class Week16 {
         }
         return sum;
     }
-
     public double sumOfColOne(double[][] array2d) {
-        //if(array2d.length > 1 && array2d[0].length > 1) return 0.0;
-
+        if(array2d.length < 1) return 0.0;
         double sumOfColumOne = 0;
-        return 1;
+
+        for (double[] array : array2d) {
+            sumOfColumOne += array[1];
+        }
+        return sumOfColumOne;
+    }
+    public double sumOfRow2(double[][] array2d) {
+        if(array2d.length < 1) return 0.0;
+        double sumOfRow2 = 0.0;
+
+        for (double value : array2d[1]) {
+            sumOfRow2 += value;
+
+        }
+        return sumOfRow2;
+    }
+    public double sumOfMatrix(double[][] array2d) {
+        if (array2d.length < 1) return 0.0;
+        double sumOfMatrix = 0.0;
+
+        for (double[] array : array2d) {
+            for (double value : array) {
+                sumOfMatrix += value;
+            }
+        }
+        return sumOfMatrix;
+    }
+    public double[][] modelExample(double[][] array2d){
+        double[][] modelExample = new double[array2d.length][1];
+        for (int index = 0; index < array2d.length; index++) {
+            array2d[index][0] = 1;
+        }
+        return array2d;
+    }
+
+    public double[][] linearClassifier(double[][] array2dX, double[] array2dW) {
+        return array2dX;
     }
 
     /*
