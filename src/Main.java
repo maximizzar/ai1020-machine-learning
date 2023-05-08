efbad6f6b8129a9ef86da39acca36feb786b3013
maximizzar <maximizzar@web.de>
2023-05-08 10:04:46 +0200

    Week19 init

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         new Week16().test();
-        new Week16Extra().test();
         new Week17().test();
         new Week18().test();
+        new Week19().test();
     }
 }
---
+++ src/Week19.java
@@ -0,0 +1,19 @@
+public class Week19 {
+    private Integer task = 1;
+    public double cosinusSteigung(double x, double eps) {
+        return x + eps;
+    }
+    public void test() {
+        printHeading();
+        System.out.printf("%.04f\n", cosinusSteigung(0, 1));
+        System.out.printf("%.04f\n", cosinusSteigung(0, 0.5));
+        System.out.printf("%.04f\n", cosinusSteigung(0, 0.1));
+    }
+
+    /*
+        extracted functions
+    */
+    private void printHeading() {
+        System.out.println("\n<> test task: " + this.task++);
+    }
+}
