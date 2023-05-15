826ac88ff0bb89355ff85635fdfc8f166cfa95a0
maximizzar <maximizzar@web.de>
2023-05-15 10:30:46 +0200

    Week20 done

--- src/Week20.java
+++ src/Week20.java
@@ -1,16 +1,31 @@
 public class Week20 {
     private Integer task = 1;
 
-    public double[] gradf(double[] array) {
+    public double[] gradf1(double[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = 2 * array[i];
         }
         return array;
     }
+    public double[] gradf2(double[] ignoredX) {
+        return new double[] {5,2};
+    }
+    public double[] gradf(double[] array) {
+        java.util.Arrays.setAll(array, i -> Math.pow(array[i], 3) * 3);
+        return array;
+    }
     public void test() {
         printHeading();
-        double[] ret = gradf(new double[] { 0., 0. });
+        double[] ret = gradf1(new double[] { 0., 0. });
         System.out.printf("%.02f %.02f\n", ret[0], ret[1]);
+
+        printHeading();
+        double[] ret2 = gradf2(new double[] { 5., 2. });
+        System.out.printf("%.02f %.02f\n", ret2[0], ret2[1]);
+
+        printHeading();
+        double[] ret3 = gradf(new double[] { 0., 0., 0. });
+        System.out.printf("%.02f %.02f %.02f\n", ret3[0], ret3[1], ret3[2]);
     }
 
     /*
